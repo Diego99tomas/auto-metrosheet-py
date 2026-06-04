36379d0728cd586d69d1ae8dad71b925ddf68268
unknown <diego99toma@gmail.com>
2026-06-04 12:20:17 -0500

    refactor: using SoC, separate connection

--- src/database/connection.py
+++ src/database/connection.py
@@ -6,7 +6,7 @@ INTERPOLATION_DB_PATH = os.path.join("data", "sqlite", "interpolation.db")
 SPECS_DB_PATH = os.path.join("data", "sqlite", "specs.db")
 
 def get_connection(db_path: str) -> sqlite3.Connection:
-    """Crea y retorna una conexión limpia a la base de datos especificada."""
+    """Crea y retorna una conexión a la base de datos especificada."""
    
     os.makedirs(os.path.dirname(db_path), exist_ok=True)
     conn = sqlite3.connect(db_path)
--- src/database/models.py
+++ src/database/models.py
@@ -1,35 +1,42 @@
-from dataclasses import dataclass
+from database.connection import get_connection
 from config.constants import Functions
 import sqlite3
 
-@dataclass
-class Create_DB():
 
-    DB_NAME:str
-    EXCEL_NAME:str
+class DataBaseBuilder:
+    """Clase base para inicializar la estructura base """
 
-    def create_db(self,value:str, unit:str, frec:str, value1:str,value2:str, optional:str|None=None):
-        conn=sqlite3.connect(self.DB_NAME)
+    def __init__(self,db_path:str):
+        self.db_path=db_path
+
+    def create_schema(self,value:str, unit:str, frec:str, value1:str,value2:str, optional:str|None=None):
+        """Crea las tablas correspondientes para cada función del equipo"""
+
+        conn=get_connection(self.db_path)
         cursor=conn.cursor()
+        try:
+            for tabla in Functions:
+                sql_query = f"""
+                CREATE TABLE IF NOT EXISTS {tabla.value} (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    {value} REAL,
+                    {unit} TEXT,
+                    {frec} TEXT,
+                    {value1} REAL,
+                    {value2} REAL
+            """ 
+                if optional:
+                    sql_query += f",\n{optional} INTEGER"
+                
+                sql_query += "\n)"
 
-        for tabla in Functions:
-            sql_query = f"""
-            CREATE TABLE IF NOT EXISTS {tabla.value} (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                {value} REAL,
-                {unit} TEXT,
-                {frec} TEXT,
-                {value1} REAL,
-                {value2} REAL
-        """ 
-            if optional:
-                sql_query += f",\n{optional} INTEGER"
-            
-            sql_query += "\n)"
+                cursor.execute(sql_query)
 
-            cursor.execute(sql_query)
-
-        print('base creada')
-        conn.commit()
-        return conn
-    
+            conn.commit()
+            print(f"Base de datos en '{self.db_path}' creada con éxito.")
+        except sqlite3.Error as e:
+            conn.rollback()
+            print(f"Error al crear la base de datos: {e}")
+            raise e
+        finally:
+            conn.close()
