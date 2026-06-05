55261267d5a0c66ad420081b5248efc745e7bdd4
unknown <diego99toma@gmail.com>
2026-06-05 10:06:42 -0500

    fix: mapping sqlite3.Row and ensuring connection closure

--- src/services/interpo_search.py
+++ src/services/interpo_search.py
@@ -1,49 +1,50 @@
-import sqlite3
+from src.database.connection import get_connection,INTERPOLATION_DB_PATH
 from utility.data_processing import normalizar_unidad,desnormalizar_unidad
 
-DB_INTERP_NAME='data/interpolation/interpolation.db'
 
 def interpol_search_in_db(type_table:str,valor:float,unidad:str,frecuencia=None):
     """Busqueda en db de puntos inferior y superior para la interpolacion"""
-    conn = sqlite3.connect(DB_INTERP_NAME)
-    cursor= conn.cursor()
-    
+
     if 'ac' in type_table and frecuencia is not None:
         if frecuencia != '60 Hz':
             frecuencia='1 kHz'
 
     busqueda = normalizar_unidad(valor,unidad)
        
-    
+    #consultas 
     query_inf = f"SELECT lectura, medida, incert FROM {type_table} WHERE lectura <= ? AND (frecuencia = ? OR frecuencia IS NULL) ORDER BY lectura DESC LIMIT 1"
 
     query_sup = f"SELECT lectura, medida, incert FROM {type_table} WHERE lectura >= ? AND (frecuencia = ? OR frecuencia IS NULL) ORDER BY lectura ASC LIMIT 1"
     
     params = (busqueda, frecuencia)
     
-    cursor.execute(query_inf, params)
-    inf = cursor.fetchone()
-    
-    cursor.execute(query_sup, params)
-    sup = cursor.fetchone()
 
-    if not inf or not sup:
-        conn.close()
-        return None 
-
-    list_inf= list(inf)
-    list_sup= list(sup)
-
-   
-    if list_inf[0]==list_sup[0]:
-        resultado=[0,0,0] + list_sup
-    else:
-        resultado = list(inf) + list(sup)
+    conn = get_connection(INTERPOLATION_DB_PATH)
     
-    
-    resultado_convertido = []
-    for val in resultado:
-        resultado_convertido.append(desnormalizar_unidad(val, unidad))
+    try:
+        cursor= conn.cursor()
+
+        with conn:
+            cursor.execute(query_inf, params)
+            inf_row = cursor.fetchone()
+            
+            cursor.execute(query_sup, params)
+            sup_row = cursor.fetchone()
+
+        if not inf_row or not sup_row:
+            return None 
+
+        inf_val=[inf_row['lectura'],inf_row['medida'],inf_row['incert']]
+        sup_val=[sup_row['lectura'],sup_row['medida'],sup_row['incert']]
+
+
+        if inf_val[0]==sup_val[0]:
+            resultado=[0,0,0] + sup_val
+        else:
+            resultado = inf_val+sup_val
         
-    return resultado_convertido
-
+        resultado_convertido = [desnormalizar_unidad(val,unidad) for val in resultado]
+        return resultado_convertido
+    
+    finally:
+        conn.close()
