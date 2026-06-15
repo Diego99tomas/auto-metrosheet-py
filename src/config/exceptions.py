215738e69ca5a625e4fc77ce18b3c457e9ee71c3
unknown <diego99toma@gmail.com>
2026-06-15 16:58:11 -0500

    feat: add more custom exceptions

--- src/config/exceptions.py
+++ src/config/exceptions.py
@@ -1,27 +1,41 @@
+class InterfaceError(Exception):
+    pass
 
-class FrequencyRequiredError(Exception):
+class MissingUnitOrValueError(InterfaceError):
+    """Se lanza cuando el valor del rango esta sin unidad o viceversa """
+    def __init__(self, func, message="El rango esta incompleto en"):
+        self.func = func
+        super().__init__(f"{message}: {func}")
+
+class MidPointOutRangeError(InterfaceError):
+    """Se lanza cuando el punto medio excede a los rangos. """
+    def __init__(self, func, message="El punto medio esta fuera de rango en"):
+        self.func = func
+        super().__init__(f"{message}: {func}")
+
+class FrequencyRequiredError(InterfaceError):
     """Se lanza cuando falta la frecuencia en una función AC."""
     def __init__(self, func, message="Se requiere frecuencia en la función"):
         self.func = func
         super().__init__(f"{message}: {func}")
 
-class FrequencyNotRequiredError(Exception):
+class FrequencyNotRequiredError(InterfaceError):
     """Se lanza cuando se incluye frecuencia en una función que no la lleva."""
     def __init__(self, func, message="No se requiere frecuencia en"):
         self.func = func
         super().__init__(f"{message}: {func}")
         
-class BobinaOnlyInCorrienteError(Exception):
+class BobinaOnlyInCorrienteError(InterfaceError):
     """Se lanza si se usa la bobina fuera de la función de Corriente."""
     def __init__(self, message="La bobina solo se usa en Corriente"):
         super().__init__(message)
 
-class DecimalValueError(Exception):
+class DecimalValueError(InterfaceError):
     """Se lanza cuando se supera el límite de decimales permitido."""
     def __init__(self, message="Verifique los decimales"):
         super().__init__(message)
 
-class IncorrectCoilUseError(Exception):
+class IncorrectCoilUseError(InterfaceError):
     """Se lanza si la bobina se usa en corrientes menores a 4A."""
     def __init__(self, message="La bobina solo se usa para valores >= 4A"):
         super().__init__(message)
